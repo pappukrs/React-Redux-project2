2f018dc285956ec446bc501bc2eee865588b5cd9
Pappu Kumar <digitalpappu101@gmail.com>
2022-07-21 01:12:55 +0530

    magic calculator completed and COMBO TODO USING REDUX

--- redux2/src/Redux/action.jsx
+++ redux2/src/Redux/action.jsx
@@ -1,7 +1,7 @@
 
 
-import { DEC_COUNT, INC_COUNT,RESET_COUNT ,DOUBLE_COUNT,HALF_COUNT,SQUARE_COUNT,CUBE_COUNT} from './actionType'
-
+import { DEC_COUNT, INC_COUNT,RESET_COUNT ,DOUBLE_COUNT,HALF_COUNT,SQUARE_COUNT,CUBE_COUNT, ADD_TODO_SUCCESS, ADD_TODO_ERROR, GET_TODO_LOADING, GET_TODO_SUCCESS, GET_TODO_ERROR, TOGGLE_TODO,DELETE_TODO} from './actionType'
+import {ADD_TODO_LOADING} from './actionType';
 export const incCount = (payload) => {
      
     return{
@@ -59,4 +59,58 @@ export const cubeCount = () => {
     };
     
 };
+
+
+
+export const addTodoLoading = () => {
+    return{
+        type:ADD_TODO_LOADING
+    }
+}
+export const addTodoSuccess = (payload) => {
+    return{
+        type:ADD_TODO_SUCCESS,
+        payload,
+    }
+}
+export const addTodoError = () => {
+    return{
+        type:ADD_TODO_ERROR,
+    }
+}
+
+export const getTodoLoading = () => {
+    return{
+        type:GET_TODO_LOADING
+    }
+}
+export const getTodoSuccess = (payload) => {
+    return{
+        type:GET_TODO_SUCCESS,
+        payload,
+    }
+}
+export const getTodoError = () => {
+    return{
+        type:GET_TODO_ERROR,
+    }
+}
+
+export const toggleTodo = (payload) => {
+    return{
+        type:TOGGLE_TODO,
+        payload,
+    }
+}
+
+export const deleteTodo = () => {
+    return{
+        type:DELETE_TODO,
+        
+    }
+}
+
+
+
+
 
